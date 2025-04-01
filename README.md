58289b00486c440ed2836176fdeaeac0a87bab2e
uranum12 <113694643+uranum12@users.noreply.github.com>
2025-04-01 11:17:04 +0900

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# seiryo sunspot library
+
+## install
+
+```sh
+pip install git+https://github.com/uranum12/seiryo-sunspot-lib
+```
---
+++ src/seiryo_sunspot_lib/__init__.py
@@ -0,0 +1,5 @@
+"""sunspot code for seiryo written in python."""
+
+from importlib.metadata import version
+
+__version__ = version(__package__)
